12a80eccd2dd9ac2f578a277494406cb446429f2
Sanatan Mishra <64330496+E-santa@users.noreply.github.com>
2025-09-20 09:25:00 -0700

    isolated background (to extent background is distinct color from highway shield)

--- week2/color-me-impressed.py
+++ week2/color-me-impressed.py
@@ -8,10 +8,12 @@ img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 mask_red_interstate = cv2.inRange(img_hsv, np.array([159, 50, 70]), np.array([180, 255, 200]))
 mask_blue_680 = cv2.inRange(img_hsv, np.array([90, 50, 70]), np.array([128, 255, 200]))
 mask_white_text = cv2.inRange(img_hsv, np.array([0, 0, 190]), np.array([180, 150, 255]))
+mask_background = cv2.inRange(img_hsv, np.array([36, 50, 70]), np.array([100, 255, 220]))
 
 
 cv2.imshow("Red Interstate Top", mask_red_interstate)
 cv2.imshow("Blue 680 Sign", mask_blue_680)
 cv2.imshow("White Text", mask_white_text)
+cv2.imshow("Background", mask_background)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
